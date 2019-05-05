55577b919734623cd35ebdcb0bd50e5e0f2d1fbd
Annie Bubinski <anbubin@microsoft.com>
2019-05-04 22:36:30 -0700

    added dialog for address auth, confirmation, phone auth, and bill total

--- dialogs/userProfileDialog.js
+++ dialogs/userProfileDialog.js
@@ -15,8 +15,10 @@ const GET_USER_PROFILE = "GET_USER_PROFILE";
 const INTENT_PROMPT = "INTENT_PROMPT";
 const LAST_NAME_PROMPT = "LAST_NAME_PROMPT";
 const FIRST_NAME_PROMPT = "FIRST_NAME_PROMPT";
+const CONFIRM_PRESCRIPTIONS_PROMPT = "CONFIRM_PRESCRIPTIONS_PROMPT";
 const ADDRESS_PROMPT = "ADDRESS_PROMPT";
 const PHONE_PROMPT = "PHONE_PROMPT";
+const CHECKOUT_PROMPT = "CHECKOUT_PROMPT";
 
 class UserProfileDialog extends ComponentDialog {
 	constructor(userState) {
@@ -30,14 +32,19 @@ class UserProfileDialog extends ComponentDialog {
 		this.addDialog(new TextPrompt(LAST_NAME_PROMPT));
 		this.addDialog(new TextPrompt(FIRST_NAME_PROMPT));
 		this.addDialog(new TextPrompt(ADDRESS_PROMPT));
+		this.addDialog(new TextPrompt(CONFIRM_PRESCRIPTIONS_PROMPT));
 		this.addDialog(new TextPrompt(PHONE_PROMPT));
+		this.addDialog(new TextPrompt(CHECKOUT_PROMPT));
 
 		// Register WaterfallDialog that will send the registered prompts
 		this.addDialog(new WaterfallDialog(GET_USER_PROFILE, [
 			this.getIntent.bind(this),
 			this.getLastName.bind(this),
 			this.getFirstName.bind(this),
-			this.collectAndDisplayName.bind(this)
+			this.getAddress.bind(this),
+			this.confirmPrescriptions.bind(this),
+			this.getPhone.bind(this),
+			this.checkout.bind(this)
 		]));
 
 		// See https://docs.microsoft.com/en-us/javascript/api/botbuilder-dialogs/componentdialog?view=botbuilder-ts-latest#method-details
@@ -71,9 +78,24 @@ class UserProfileDialog extends ComponentDialog {
 		return await step.prompt(FIRST_NAME_PROMPT, "What is your first name?");
 	}
 
-	async collectAndDisplayName(step) {
+	async getAddress(step) {
 		this.userProfile.firstName = step.result;
 		await step.context.sendActivity("Hi there, " + this.userProfile.firstName + " " + this.userProfile.lastName + "!");
+		return await step.prompt(ADDRESS_PROMPT, "Can you please validate the address on the account?");
+	}
+
+	async confirmPrescriptions(step) {
+		this.userProfile.address = step.result;
+		return await step.prompt(CONFIRM_PRESCRIPTIONS_PROMPT, "I have three prescriptions for pick up. Are you aware there is a large copay on the naratriptan of $195?");
+	}
+
+	async getPhone(step) {
+		return await step.prompt(PHONE_PROMPT, "Please provide the last 4 digits of the phone number on the account.");
+	}
+
+	async checkout(step) {
+		this.userProfile.phone = step.result;
+		await step.context.sendActivity("Your total is $215. Please put your payment in the pin and press the send button.");
 		return await step.endDialog();
 	}
 }
